b5fe489ee637f86ff44454cb5442ded24f8aeb4d
alisedighi <ali.sedighi@gmail.com>
2014-08-24 13:00:49 -0700

    adding phone number validation

--- js/vcn-validation.js
+++ js/vcn-validation.js
@@ -4,13 +4,17 @@ var $j = jQuery.noConflict();
 $j(document).ready(function(){
    var validation = $j('#signup').validate({
    			rules : {
-        		names:{required:true,maxlength:40}                   
+        		names:{required:true,maxlength:40},
+			phone1:{required:true,phoneUS:true}                   
         }//end of the rules
         ,
         messages : {
 		        names:{
 				required:"You need to enter your name",
 				maxlength:"your name can not be more than 40 characters"
+			},
+			phone1:{
+				required:"you need to enter your phone number"
 			}
         }//end of the messages  
    });//end of validate
