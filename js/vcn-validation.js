479aa72081767d32a4c102899f35dba3186ed9f9
alisedighi <ali.sedighi@gmail.com>
2014-08-27 13:10:41 -0700

    Adding username validation
    Login name should started by a letter and followed by either leters or
    digits. The range of the username could be between 3 to 8 characters. No
    uppercase letter or punctuation is allowed.

--- js/vcn-validation.js
+++ js/vcn-validation.js
@@ -15,11 +15,15 @@ $j.validator.addMethod('validFullname',function(value,element){
 		return (this.optional(element)||/^[A-z0-9\s\(\)\'\#\.\,\-\/\\]+\s([A-z0-9\(\)\'\#\.\,\-\/\\]+)$/.test(value));
 },"Please enter your first and last name");
 
+$j.validator.addMethod('validUser',function(value,element){
+		return(this.optional(element)||/^[a-z][a-z0-9]{2,7}$/.test(value));
+},"Please eneter a username started by a lowercase letter and between 3 to 8 either letters or numbers.");
+
    var validation = $j('#signup').validate({
    			rules : {
         		names:{required:true,validFullname:true ,maxlength:40},
 			phone1:{required:true,phoneUS:true},
-			login:{required:true,remote:"/cgi-bin/vcn-validationuser.cgi"},
+			login:{required:true,validUser:true,remote:"/cgi-bin/vcn-validationuser.cgi"},
 			password:{required:true,rangelength:[6,8]},
 			birth_contact:{required:true, validDate:true},
 			keyword:{required:true},
