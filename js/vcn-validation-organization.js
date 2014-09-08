e1ed7d5c74964f4f2ca7968e1f5d2fd38d222cc3
ali sedi <ali.sedighi@gmail.com>
2014-09-07 21:33:54 -0700

    Adding contact name validation
    This resolves #25, fixes #18 by adding a static validation for
    contact name filed.

--- js/vcn-validation-organization.js
+++ js/vcn-validation-organization.js
@@ -7,13 +7,16 @@ $j.validator.addMethod('validZip',function(value,element){
 		return (this.optional(element) || /^[ABCEGHJKLMNPRSTVXYabcedhjklmnprstvxy]{1}\d{1}[A-z]{1} *\d{1}[A-z]{1}\d{1}$/.test(value));
 },"Please enter a valid postal code");
 
+$j.validator.addMethod('validContact',function(value,element){
+	return (this.optional(element)||/^[A-z0-9\s\(\)\'\#\.\,\-\/\\]+\s([A-z0-9\(\)\'\#\.\,\-\/\\]+)$/.test(value));},"Please enter a valid contact name");
+ 
    var validation = $j('#signup').validate({
    			rules : {
         	names:{required:true,maxlength:40},
 			phone1:{required:true,phoneUS:true},
 			login:{required:true,remote:"/cgi-bin/vcn-validationuser.cgi"},
 			password:{required:true,rangelength:[6,8]},
-			birth_contact:{required:true},
+			birth_contact:{required:true,validContact:true},
 			keyword:{required:true},
 			address1:{required:true},
 			address2:{required:true},
@@ -44,7 +47,7 @@ $j.validator.addMethod('validZip',function(value,element){
                                rangelength:"Your password may be between 6 to 8 characters"
 			},
 			birth_contact:{
-				required:"You need to enter your birth date"
+				required:"You need to enter a contact name"
 			},
 			keyword:{
 				required:"You need to enter a keyword"
