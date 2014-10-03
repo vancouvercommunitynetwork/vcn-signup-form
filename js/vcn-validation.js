211b1523e8b04579ef50148733e2fc90c526e732
alisedi <ali.sedighi@gmail.com>
2014-10-02 20:51:17 -0700

    Finalize the email-sign-up validation
    oses #4,closes #5,closes #9,closes #10,closes #13

--- js/vcn-validation.js
+++ js/vcn-validation.js
@@ -4,7 +4,7 @@ var $j = jQuery.noConflict();
 $j(document).ready(function(){
 $j.validator.addMethod('validPass',function ( value ,element){
                  return (this.optional(element)||/^(?=.*[0-9])(?=.*[A-z])([A-z0-9\(\)\ \'\#\.\,\-\/\\]){6,8}$/.test(value) );
-},"Please enter a password whit at least one letter and one digit. You can use these characters in your password as well \(\)\'\#\.\,\-\/ \\ and space");
+},"Please enter a password with at least one letter and one digit. You can use these characters in your password as well \(\)\'\#\.\,\-\/ \\ and space");
 
 $j.validator.addMethod('validDate',function ( value ,element){
                 return (this.optional(element)|| /^(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])\/((19|20)\d\d)$/.test(value) );
@@ -30,11 +30,7 @@ $j.validator.addMethod('validUser',function(value,element){
 			password:{required:true,rangelength:[6,8],validPass:true},
 			birth_contact:{required:true, validDate:true},
 			keyword:{required:true},
-			address1:{required:true},
-			city:{required:true},
-			province:{required:true},
-			postalcode:{required:true,validZip:true},
-			country:{required:true},			
+			postalcode:{validZip:true},
         }//end of the rules
         ,
         messages : {
@@ -58,21 +54,6 @@ $j.validator.addMethod('validUser',function(value,element){
 			keyword:{
 				required:"You need to enter a keyword"
 			},
-			address1:{
-				required:"You need to enter your address"
-			},
-			city:{
-				required:"You need to enter the name of your city"
-			},
-			province:{
-				required:"You need to enter the name of your province"
-			},
-			postalcode:{
-				required:"You need to enter your postal code"
-			},
-			country:{
-				required:"You should enter the anme of your countery"
-			}
         }//end of the messages  
    });//end of validate
 	$j('#reset').click(function(){
